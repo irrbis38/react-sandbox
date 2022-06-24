77ee8c08aeecc0600bdf4cde9d971cd9e84cdd02
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2022-06-24 14:10:34 +0300

    feat: module 3 completed

--- ts-ms/module-3/build/module-3.js
+++ ts-ms/module-3/build/module-3.js
@@ -1,18 +1,38 @@
 "use strict";
 /** Интерфейс */
 let employee = {
-    firstName: 'Emil',
-    lastName: 'Andersson',
+    firstName: "Emil",
+    lastName: "Andersson",
     fullName() {
-        return this.firstName + ' ' + this.lastName;
+        return this.firstName + " " + this.lastName;
     },
 };
 console.dir(employee);
 let employeeWithType = {
-    firstName: 'Emil',
-    lastName: 'Andersson',
+    firstName: "Emil",
+    lastName: "Andersson",
     fullName() {
-        return this.firstName + ' ' + this.lastName;
+        return this.firstName + " " + this.lastName;
     },
 };
 console.dir(employeeWithType);
+let iceCream = {
+    flavor: "vanilla",
+    scoops: 2,
+    sauce: "chocolate",
+    whippedCream: true,
+};
+console.log(iceCream.flavor);
+function tooManyScoops(dessert) {
+    if (dessert.scoops >= 4) {
+        return dessert.scoops + " is too many scoops!";
+    }
+    else {
+        return "Your order will be ready soon!";
+    }
+}
+console.log(tooManyScoops({ flavor: "vanilla", scoops: 5, sauce: "caramel" }));
+let myIceCream;
+myIceCream = ["chocolate", "vanilla", "strawberry"];
+let myStr = myIceCream[2];
+console.log(myStr);
--- ts-ms/module-3/module-3.ts
+++ ts-ms/module-3/module-3.ts
@@ -7,10 +7,10 @@ interface Employee {
 }
 
 let employee: Employee = {
-  firstName: 'Emil',
-  lastName: 'Andersson',
+  firstName: "Emil",
+  lastName: "Andersson",
   fullName(): string {
-    return this.firstName + ' ' + this.lastName;
+    return this.firstName + " " + this.lastName;
   },
 };
 
@@ -26,11 +26,52 @@ type EmployeeType = {
 };
 
 let employeeWithType: EmployeeType = {
-  firstName: 'Emil',
-  lastName: 'Andersson',
+  firstName: "Emil",
+  lastName: "Andersson",
   fullName(): string {
-    return this.firstName + ' ' + this.lastName;
+    return this.firstName + " " + this.lastName;
   },
 };
 
 console.dir(employeeWithType);
+
+interface IceCream {
+  flavor: string;
+  scoops: number;
+  instruction?: string;
+}
+
+interface Sundae extends IceCream {
+  sauce: "chocolate" | "caramel" | "strawberry";
+  nuts?: boolean;
+  whippedCream?: boolean;
+  instructions?: string;
+}
+
+let iceCream: Sundae = {
+  flavor: "vanilla",
+  scoops: 2,
+  sauce: "chocolate",
+  whippedCream: true,
+};
+
+console.log(iceCream.flavor);
+
+function tooManyScoops(dessert: Sundae) {
+  if (dessert.scoops >= 4) {
+    return dessert.scoops + " is too many scoops!";
+  } else {
+    return "Your order will be ready soon!";
+  }
+}
+
+console.log(tooManyScoops({ flavor: "vanilla", scoops: 5, sauce: "caramel" }));
+
+interface IceCreamArray {
+  [index: number]: string;
+}
+
+let myIceCream: IceCreamArray;
+myIceCream = ["chocolate", "vanilla", "strawberry"];
+let myStr: string = myIceCream[2];
+console.log(myStr);
