05f8ac6b3855cd9e2b9f0a7f90f9685bcb1fcea9
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2022-07-08 16:46:04 +0300

    feat: update Main and Search to functional components

--- react-films-mn/src/layout/Main/Main.jsx
+++ react-films-mn/src/layout/Main/Main.jsx
@@ -7,61 +7,51 @@ import Filter from "../../components/Filter/Filter";
 
 import styles from "./Main.module.css";
 
-class Main extends React.Component {
-  state = {
-    films: [],
-    search: "comedy",
-    filter: "",
-  };
+function Main() {
+  const [films, setFilms] = React.useState([]);
+  const [search, setSearch] = React.useState("comedy");
+  const [filter, setFilter] = React.useState("");
 
-  getPosts = () => {
+  const getPosts = () => {
     fetch(
-      `https://www.omdbapi.com/?i=tt3896198&apikey=56f4f48e&s=${this.state.search}&type=${this.state.filter}`
+      `https://www.omdbapi.com/?i=tt3896198&apikey=56f4f48e&s=${search}&type=${filter}`
     )
       .then((res) => res.json())
       .then((moviesData) => {
         if (moviesData.Response === "True") {
-          this.setState({ films: moviesData.Search });
+          setFilms(moviesData.Search);
         } else {
-          this.setState({ films: null });
+          setFilms(null);
         }
       });
   };
 
-  handleSearch = (e) => {
-    this.setState({ search: e.target.value });
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
   };
 
-  handleChangeFilter = (e) =>
-    this.setState({ filter: e.target.value }, () => {
-      this.getPosts();
-    });
+  const handleChangeFilter = (e) => {
+    setFilter(e.target.value);
+  };
 
-  componentDidMount() {
-    this.getPosts();
-  }
+  React.useEffect(() => {
+    getPosts();
+  }, [filter]);
 
-  render() {
-    const { films, search, filter } = this.state;
-    return (
-      <main className={styles.main}>
-        <h1 className={styles.title}>You can find any movies description</h1>
-        <Search
-          getPosts={this.getPosts}
-          handleSearch={this.handleSearch}
-          search={search}
-        />
-        <Filter handleChangeFilter={this.handleChangeFilter} filter={filter} />
-        {!films ? (
-          <h3>Nothig fount</h3>
-        ) : films.length ? (
-          <Movies films={films} />
-        ) : (
-          <Preloader />
-        )}
-      </main>
-    );
-  }
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>You can find any movies description</h1>
+      <Search getPosts={getPosts} handleSearch={handleSearch} search={search} />
+      <Filter handleChangeFilter={handleChangeFilter} filter={filter} />
+      {!films ? (
+        <h3>Nothig fount</h3>
+      ) : films.length ? (
+        <Movies films={films} />
+      ) : (
+        <Preloader />
+      )}
+    </main>
+  );
 }
 
 export default Main;
